971a74a27cb8a9b5739849d50d51beab3d24333c
shahrour <ahmadshahrour951@gmail.com>
2021-02-26 22:12:27 +0400

    Create_arr

--- HashTable.py
+++ HashTable.py
@@ -1,3 +1,11 @@
 from LinkedList import LinkedList
 
 
+class HashTable:
+    def __init__(self, size):
+        self.size = size
+        self.arr = self.create_arr(size)
+
+    def create_arr(self, size):
+        # This function uses the map method to generate an arrat of linkedlists
+        return list(map(lambda x: LinkedList(), [None] * size))
