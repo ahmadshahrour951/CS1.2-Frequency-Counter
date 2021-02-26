b2dd263f073a4692b48a7648e980f652db5ca3d0
shahrour <ahmadshahrour951@gmail.com>
2021-02-26 22:12:36 +0400

    hash_func

--- HashTable.py
+++ HashTable.py
@@ -9,3 +9,8 @@ class HashTable:
     def create_arr(self, size):
         # This function uses the map method to generate an arrat of linkedlists
         return list(map(lambda x: LinkedList(), [None] * size))
+
+    def hash_func(self, key):
+        # This function is the logic to generate a key to store the item in a linked list, it uses ord and % to loop through the list if the letter key[0] is larger than self.size
+        return (ord(key[0]) - ord('a')) % self.size
+
